7e5ec511633e17c517f8d1a389322ed66cd17168
Keke Zhou <kejin.zhou@aalto.fi>
2024-04-05 15:33:31 +0300

    adds hg <-> txt conversion

--- py-scripts/helper_functions.py
+++ py-scripts/helper_functions.py
@@ -2,7 +2,6 @@ from time      import struct_time, strftime
 
 ALT_FILE_SUFFIX_NUMBER = 1
 
-### maths methods
 # helper: calculate  a! / b!
 def factorial_div_factorial(top, bot):
     assert type(top) == int
@@ -61,7 +60,6 @@ def get_filename_suffix(fname, ch = None):
             last = j
     stripped = fname[last_:]
     return stripped
-    
 ### Dates
 #
 def get_date_custom():
@@ -107,6 +105,35 @@ def write_to_file(fname, data):
         file.write(data)
         file.close()
 
+# converts a hypergraph object to text form
+def dict_fs_to_txt(dict_to_save) -> str:
+    D = dict_to_save
+    out_str = ""
+    for pair in D.items():
+        for element in pair[0]:
+            out_str += str(element) + ' '
+        out_str += str(pair[1]) + '\n'
+    return out_str
+
+# opens a file and reads the hypergraph data to object variable
+def read_dict_fs_to_var(filename, dict_var) -> bool:
+    with open(filename, mode='rt', encoding="utf-8") as fr:
+        line = fr.readline()
+        # parse line and convert back to hyperedge
+        while(line):
+            split = line.split(' ')
+            s = set()
+            for element in split:
+                try: 
+                    s.add(int(element))
+                except:
+                    s = set()
+                    break
+            if len(s) >= 2:
+                n = s.pop()
+                dict_var[frozenset(s)] = n
+            line = fr.readline()
+    return True
 ################################
 
 # def print_list_magnitudes(list, end = None):
