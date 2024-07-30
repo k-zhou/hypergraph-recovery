64eddf87ceaf74763c4b9b1ff5ce3496cdd07e9c
Kejin Zhou <kejin.zhou@aalto.fi>
2024-07-30 12:21:54 +0300

    shuffling and cleaning up

--- py-scripts/Hypergraph_Reconstructor.py
+++ py-scripts/Hypergraph_Reconstructor.py
@@ -614,26 +614,10 @@ class Hypergraph_Reconstructor:
 
         return
 
-    ## Observation: as of this 28.11.2022 implementation, the algorithm tends to endlessly add 2-edges. This method cuts down on the number of repeated hyperedges to a given maximum number
-    ## returns a number (int) of all those pruned away
-
-    ## edit 1: or no, it doesn't now. What is going on? Now it's stabilizing around some values
-    #  edit 2: Think I've found an odd bug. After loading up the file into the object you'd normally need to use the method init_hypergraph() to set the initial state for the algorithm to run on. If you do this, the test graph windsurfers.gt will stabilize to some hypergraph of 800 2-edges. If you don't initialize and try to run the algorithm, it'll throw an error as per planned; but if you then initialize it and run the recovery algorithm, it will "stabilize" around 400 2-edges 430 3-edges. The 3-edges part is stable but the 2-edges part will endlessly rise in count.
-
-    def prune_hypergraph(self, threshold) -> int:
-
-        assert type( threshold)  == int and threshold >= 0
-
-        stats = 0
-        for hyperedge in self._current_hypergraph:
-            count = self._current_hypergraph[ hyperedge]
-            if count > threshold:
-                stats += count - threshold
-                self._current_hypergraph[ hyperedge] = threshold
-        print(f"{stats} hyperedges pruned")
-        return stats
+    ##### auxilliary methods #####
     
-    # logging
+    ## logging
+
     def output_to_log(self, fname = None) -> None:
         if fname == None:
             fname = self._file_path + self._filename_only + "(log)" + ".txt"
@@ -677,8 +661,6 @@ class Hypergraph_Reconstructor:
         write_to_file(fname, data_to_write)
         return
     
-    ##### auxilliary methods #####
-
     def add_to_history(self, str_data = "") -> None:
         lines = [] # allows for passing lists of strings
         lines.append(str_data)
@@ -720,8 +702,29 @@ class Hypergraph_Reconstructor:
 
     ## Prints the current state of the hypergraph
     def status(self):
-        print(f"--- Status of {self._filename} ---")
-        print(f"Current iteration: {self._iteration}")
-        print(f"Edges of size k: {self._E_current}")
-        print(f"Auto-stop state: {self._stopping_arr}\n rw_index {self._rw_index} ; stopping sum {self._stopping_sum}")
-        print(f"Total runtime: {self._runtime} ns or {self._runtime / 1000000} ms")
+        s = f"--- Status of {self._filename} ---\n"    + \
+            f"Current iteration: {self._iteration}\n"  + \
+            f"Edges of size k: {self._E_current}\n"    + \
+            f"Auto-stop state: {self._stopping_arr}\n rw_index {self._rw_index} ; stopping sum {self._stopping_sum}\n" + \
+            f"Total runtime: {self._runtime} ns or {self._runtime / 1000000} ms\n" + \
+            f"--------------------------------\n"
+        print(s)
+
+    ## Observation: as of this 28.11.2022 implementation, the algorithm tends to endlessly add 2-edges. This method cuts down on the number of repeated hyperedges to a given maximum number
+    ## returns a number (int) of all those pruned away
+
+    ## edit 1: or no, it doesn't now. What is going on? Now it's stabilizing around some values
+    #  edit 2: Think I've found an odd bug. After loading up the file into the object you'd normally need to use the method init_hypergraph() to set the initial state for the algorithm to run on. If you do this, the test graph windsurfers.gt will stabilize to some hypergraph of 800 2-edges. If you don't initialize and try to run the algorithm, it'll throw an error as per planned; but if you then initialize it and run the recovery algorithm, it will "stabilize" around 400 2-edges 430 3-edges. The 3-edges part is stable but the 2-edges part will endlessly rise in count.
+
+    # def prune_hypergraph(self, threshold) -> int:
+
+    #     assert type( threshold)  == int and threshold >= 0
+
+    #     stats = 0
+    #     for hyperedge in self._current_hypergraph:
+    #         count = self._current_hypergraph[ hyperedge]
+    #         if count > threshold:
+    #             stats += count - threshold
+    #             self._current_hypergraph[ hyperedge] = threshold
+    #     print(f"{stats} hyperedges pruned")
+    #     return stats
