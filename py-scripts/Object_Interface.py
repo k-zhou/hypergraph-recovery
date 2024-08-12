ba2b3107a4e3f69ccb95e8d9893cb8ffc4c49689
Kejin Zhou <kejin.zhou@aalto.fi>
2024-08-12 17:53:13 +0300

    adds Object_Interface, changes main() to - use this object instead

---
+++ py-scripts/Object_Interface.py
@@ -0,0 +1,131 @@
+import os
+from Hypergraph_Reconstructor import *
+from fnmatch                  import fnmatch
+
+class Object_Interface:
+
+    def __init__(self) -> None:
+
+        self._dataset_location = "../source-data/"
+        self._output_location  = "../output/"
+        self._container        = []
+        self._current          = 0
+
+        s = "-- Hypergraph Reconstruction from Network Data --\n" + \
+        f"Taking datasets from the folder: \"{self._dataset_location}\" \n" + \
+         "(Use change_dataset_location() to change this.)\n" + \
+        f"Outputting to folder: \"{self._output_location}\" \n" + \
+         "(Use change_output_location() to change this.)\n" + \
+        f"> run load_file() to start\n" + \
+        "> run load_all()  to load all .gt files in the dataset folder\n"
+        print(s)
+
+    def get_dataset_location(self) -> str:
+        return self._dataset_location
+    
+    def get_output_location(self) -> str:
+        return self._output_location
+    
+    def change_dataset_location(self, new=None) -> None:
+        if new == None:
+            new = input("Enter the new path to the datasets folder:\n")
+        self._dataset_location = new
+
+    def change_output_location(self, new=None) -> None:
+        if new == None:
+            new = input("Enter the new path to the outputs folder:\n")
+        self._output_location = new
+
+    def get_container_len(self) -> int:
+        return len(self._container)
+
+    def load_file(self, filename = None) -> None:
+        if filename == None:
+            filename = input(f"Enter the file / path to file. Current location is\n" + \
+                             f"{self._dataset_location}")
+        self._container.append( Hypergraph_Reconstructor( self._dataset_location + filename) )
+
+    # loads all .gt files in the DATASET LOCATION folder to self._container
+    def load_all(self) -> None:
+        file_list = []
+        len_before = len(self._container)
+        counter = 0
+        for filename in os.listdir(self._dataset_location):
+            if fnmatch(filename, "*.gt"):
+                file_list.append(filename)
+        for item in file_list:
+            print(f"[{len_before + counter}] {item}")
+            self.load_file(item)
+            counter += 1
+        print(f"{len(file_list)} files appended to self._container[{len_before}+{len(file_list)}]")
+
+    def set_current(self,  target = None ) -> bool:
+        if not target == None and 0 <= target and target < len(self._container):
+            self._current = target
+            print(f"---- ---- ---- ----\nSELECTED [{self._current}] : {self._container[self._current]._filename_only}")
+            return True
+        else:
+            print(f"CANNOT SELECT [{target}]")
+            return False
+
+    # runs for a set amount of iterations default 100
+    def run_algorithm(self, max_iterations = None, autostop = None, min_iterations = None) -> None:
+        if min_iterations == "again":
+            n = 2*self._container[self._current]._iteration +1
+            print(f"Running again for a minimum of {n} iterations.")
+            min_iterations = n
+        elif not min_iterations == None:
+            min_iterations = int(min_iterations)
+        else:
+            min_iterations = 100
+        if len(self._container) > 0:
+            print(f"Running on self._container[{self._current}]. Target {max_iterations} (default 100) iterations.")
+            self._container[self._current].run_algorithm( max_iterations, autostop, min_iterations)
+            print(f"Stopped")
+        else:
+            print("No files loaded.")
+
+    #
+    def run_until_autostopped(self, min_iterations = None ) -> None:
+        r1 = self._container[self._current]
+        while not r1._auto_stopped:
+            self.run_algorithm(10000, True, min_iterations)
+            r1.status()
+        sleep(1)
+        r1._auto_stopped = False
+
+    #
+    def run_forced(self, iterations) -> None:
+        r1 = self._container[self._current]
+        self.run_algorithm(iterations, False)
+        r1.status()
+        self.save_output()
+
+    # saves the log to file
+    def save_output(self) -> None:
+        rec = self._container[self._current]
+        fn  = self._output_location + rec._filename_only + "(log)" + ".txt"
+        rec.output_to_log(fn)
+        print(f"Saved to {fn}")
+        return
+
+    def save_history(self) -> None:
+        rec = self._container[self._current]
+        fn  = self._output_location + rec._filename_only + "(history)" + ".txt"
+        rec.output_history_to_log(fn)
+        print(f"Saved to {fn}")
+        return
+
+    def save_history_exact(self) -> None:
+        rec = self._container[self._current]
+        fn  = self._output_location + rec._filename_only + "(history_exact)" + ".txt"
+        rec.output_history_exact_to_log(fn)
+        print(f"Saved to {fn}")
+        return
+
+    def save_hypergraph(self) -> None:
+        rec = self._container[self._current]
+        fn  = self._output_location + rec._filename_only + "(h_graph)" + ".txt"
+        rec.output_hypergraph_to_log(fn)
+        print(f"Saved to {fn}")
+        return
--- py-scripts/main.py
+++ py-scripts/main.py
@@ -9,137 +9,20 @@
 # math.factorial(x)
 
 # import string
-from Hypergraph_Reconstructor import *
+
+from Object_Interface import *
 from project_tests   import *
-import os
-from fnmatch         import fnmatch
 
 ### Main ##########
 
-# plan:
-## load files from pre-determined source files folder
-## output results to txt file, including
-##  runtime, initial and final h-graph h-edges count,
-
-def main():
-
-    # global variables 
-    global help
-    global FILE_LOADED, DATASET_LOCATION, OUTPUT_LOCATION, RECONSTRUCTORS, CURRENT
-    FILE_LOADED        = False
-    DATASET_LOCATION   = "../source-data/"
-    OUTPUT_LOCATION    = "../output/"
-    RECONSTRUCTORS     = []
-    CURRENT            = 0
-    #help               = help()
-
-    print("\n-- Hypergraph Reconstruction from Network Data --")
-    print(f"Taking datasets from the folder: \"{DATASET_LOCATION}\" \nModify global variable DATASET_LOCATION to change this.")
-    print(f"Outputting to folder: \"{OUTPUT_LOCATION}\" \nModify global variable OUTPUT_LOCATION to change this.")
-    print("> run load_file() to start")
-    print("> run load_all()  to load all .gt files in the dataset folder")
-    #hypergraph   = init_hypergraph(G_orig)
-    #graph_order  = len(list(G_orig.vertex_index))
-    #results      = None
-
-    return
-
-#
-
-def load_file(filename = None):
-    global DATASET_LOCATION, RECONSTRUCTORS
-    if filename == None:
-        filename      = input(f"Enter the file / path to file: {DATASET_LOCATION}")
-        RECONSTRUCTORS.append( Hypergraph_Reconstructor( DATASET_LOCATION + filename) )
-        #print(f"Loaded file { filename} to object \"RECONSTRUCTORS[{ len(RECONSTRUCTORS) - 1 }]\"")
+def main() -> None:
+    if not "Reconstructor" in globals():
+        global Reconstructor
+        print("Creating object \"Reconstructor\" ...")
+        Reconstructor = Object_Interface()
     else:
-        RECONSTRUCTORS.append( Hypergraph_Reconstructor( DATASET_LOCATION + filename) )
+        print("Needs variable \"Reconstructor\" to be available to start.")
 
-# loads all .gt files in the DATASET LOCATION folder to RECONSTRUCTORS
-def load_all():
-    global DATASET_LOCATION, RECONSTRUCTORS
-    file_list = []
-    len_before = len(RECONSTRUCTORS)
-    counter = 0
-    for filename in os.listdir(DATASET_LOCATION):
-        if fnmatch(filename, "*.gt"):
-            file_list.append(filename)
-    for item in file_list:
-        print(f"[{len_before + counter}] {item}")
-        load_file(item)
-        counter += 1
-    print(f"{len(file_list)} files appended to RECONSTRUCTORS[{len_before}+{len(file_list)}]")
-
-def set_current( target = None ) -> bool:
-    global RECONSTRUCTORS
-    if not target == None and 0 <= target and target < len(RECONSTRUCTORS):
-        global CURRENT
-        CURRENT = target
-        print(f"---- ---- ---- ----\nSELECTED [{CURRENT}] : {RECONSTRUCTORS[CURRENT]._filename_only}")
-        return True
-    else:
-        print(f"CANNOT SELECT [{target}]")
-        return False
-
-# runs for a set amount of iterations default 100
-def run_algorithm( max_iterations = None, autostop = None, min_iterations = None):
-    global RECONSTRUCTORS, CURRENT
-    if len(RECONSTRUCTORS) > 0:
-        print(f"Running on RECONSTRUCTORS[{CURRENT}]. Target {max_iterations} (default 100) iterations.")
-        RECONSTRUCTORS[CURRENT].run_algorithm( max_iterations, autostop, min_iterations)
-        print(f"Stopped")
-    else:
-        print("No files loaded.")
-
-#
-def run_until_autostopped( min_iterations = None ) -> None:
-    global RECONSTRUCTORS, CURRENT
-    r1 = RECONSTRUCTORS[CURRENT]
-    while not r1._auto_stopped:
-        run_algorithm(10000, True, min_iterations)
-        r1.status()
-    sleep(1)
-    r1._auto_stopped = False
-
-#
-def run_forced(iterations) -> None:
-    global RECONSTRUCTORS, CURRENT
-    r1 = RECONSTRUCTORS[CURRENT]
-    run_algorithm(iterations, False)
-    r1.status()
-    save_output()
-
-# saves the log to file
-def save_output() -> None:
-    global RECONSTRUCTORS, CURRENT, OUTPUT_LOCATION
-    rec = RECONSTRUCTORS[CURRENT]
-    fn  = OUTPUT_LOCATION + rec._filename_only + "(log)" + ".txt"
-    rec.output_to_log(fn)
-    print(f"Saved to {fn}")
-    return
-
-def save_history() -> None:
-    global RECONSTRUCTORS, CURRENT, OUTPUT_LOCATION
-    rec = RECONSTRUCTORS[CURRENT]
-    fn  = OUTPUT_LOCATION + rec._filename_only + "(history)" + ".txt"
-    rec.output_history_to_log(fn)
-    print(f"Saved to {fn}")
-    return
-
-def save_history_exact() -> None:
-    global RECONSTRUCTORS, CURRENT, OUTPUT_LOCATION
-    rec = RECONSTRUCTORS[CURRENT]
-    fn  = OUTPUT_LOCATION + rec._filename_only + "(history_exact)" + ".txt"
-    rec.output_history_exact_to_log(fn)
-    print(f"Saved to {fn}")
-    return
-
-def save_hypergraph() -> None:
-    global RECONSTRUCTORS, CURRENT, OUTPUT_LOCATION
-    rec = RECONSTRUCTORS[CURRENT]
-    fn  = OUTPUT_LOCATION + rec._filename_only + "(h_graph)" + ".txt"
-    rec.output_hypergraph_to_log(fn)
-    print(f"Saved to {fn}")
     return
 
 def help():
@@ -152,19 +35,3 @@ def help():
 if __name__ == "__main__":
     main()
 
-"""
-# run the algorithm
-
-FILENAME = "../source_data/windsurfers.gt"
-G_orig   = init( FILENAME )
-if (G_orig == None): raise Exception("Cannot continue with null as the graph G_orig.")
-print("Loaded file", FILENAME, "\nrunning algorithm...")
-try:
-    result = find_best_hypergraph( G_orig)
-except:
-    print("Error.")
-
-# Visualisation
-
-stop_execution = input("Algorithm finished. Press any key to exit...")
- """
